86d4e1f4601303cf0a4334be20bae2ebcefbcdc0
TeodorStS <tedistoyanov06ie@gmail.com>
2025-11-26 16:27:01 +0000

    js part 2

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('forminfo');
+const firstnamefield = document.getElementById('fname');
+const secondnamefield = document.getElementById('sname');
+const phonenumberfield = document.getElementById('phonenum');
+const emailfield = document.getElementById('email');
+const queryfield = document.getElementById('Query');
+
+const nameValue = firstnamefield.value;
+const firstnameValue = firstnamefield.value;
+const secondnameValue = secondnamefield.value;
+const phonenumValue = phonenumberfield.value;
+const emailValue = emailfield.value;
+const queryValue = queryfield.value;
+
