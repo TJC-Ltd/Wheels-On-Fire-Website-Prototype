0adca380a771db72ff692a796906fc5bc863a983
TeodorStS <tedistoyanov06ie@gmail.com>
2025-11-27 16:25:11 +0000

    all of js complete

--- JS/index.js
+++ JS/index.js
@@ -4,11 +4,48 @@ const secondnamefield = document.getElementById('sname');
 const phonenumberfield = document.getElementById('phonenum');
 const emailfield = document.getElementById('email');
 const queryfield = document.getElementById('Query');
+const charCounter = document.getElementById('charCounter');
 
-const nameValue = firstnamefield.value;
-const firstnameValue = firstnamefield.value;
-const secondnameValue = secondnamefield.value;
-const phonenumValue = phonenumberfield.value;
-const emailValue = emailfield.value;
-const queryValue = queryfield.value;
+function isEmail(value) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(value);
+}
+
+function isPhoneNumber(value) {
+    const numberregex = /^\d{7,15}$/;
+    return numberregex.test(value);
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const firstnameValue = firstnamefield.value;
+    const secondnameValue = secondnamefield.value;
+    const phonenumValue = phonenumberfield.value;
+    const emailValue = emailfield.value;
+    const queryValue = queryfield.value;
+
+    if (!isEmail(emailValue)) {
+        alert("Invalid email");
+        return;
+    }
+
+    if (!isPhoneNumber(phonenumValue)) {
+        alert("Invalid phone number");
+        return;
+    }
+
+    if (queryValue.length > 300) {
+        alert("Your query must be 300 characters or less.");
+        return;
+    }
+
+    alert("Form submitted successfully!");
+});
+
+
+queryfield.addEventListener('input', function () {
+    const length = queryfield.value.length;
+    charCounter.textContent = `${length} / 300`;
+});
 
